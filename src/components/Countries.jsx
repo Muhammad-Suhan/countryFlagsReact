2e834f0cf94abcecb55eef3ef5448e18c64f2e91
suhan <dev.muhammad.suhan@gmail.com>
2025-11-18 00:16:56 +0600

    adding toggle btn

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,24 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import Country from './country';
+import React, { useEffect, useState } from "react";
+import Country from "./country";
 
 const Countries = () => {
-    const [countries, setCountries] = useState([])
-    useEffect(()=>{
-        fetch('https://restcountries.com/v3.1/independent?status=true')
-        .then(res => res.json())
-        .then(data=> setCountries(data))
-    },[])
-    
-    return (
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/independent?status=true")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+
+  return (
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-4 mt-4">
+        <h1 className="text-2xl font-bold">
+          Countries: {countries.length}
+        </h1>
         <div>
-            <h1 className='text-center text-2xl font-bold'>Countries: {countries.length}</h1>
-            <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5'>
-            {
-                countries.map(country => <Country country={country}></Country>)
-            }
-            </div>
+          <label className="input input-bordered flex items-center gap-2 w-4/5">
+            <input type="text" className="grow" placeholder="Search" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4 opacity-70"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </label>
         </div>
-    );
+      </div>
+      
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
+        {countries.map((country) => (
+          <Country country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
--- src/components/country.jsx
+++ src/components/country.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Country = ({country}) => {
-    console.log(country);
-    const {name, capital, region, subregion, population, flags} = country;
+    const {name, capital, region, subregion, population, flags, cca3} = country;
+
+    const [visited, setVisited] = useState(false);
+    const handleVisited = ()=>{
+        setVisited(!visited);
+    }
     return (
         <div className='border-2 border-green-500 m-4 p-4 rounded-lg bg-green-100 text-black flex flex-col justify-around'>
             <div>
@@ -10,9 +14,12 @@ const Country = ({country}) => {
             </div>
             <h2 className="text-2xl">Name: {name.common? name.common : name.official}</h2>
             <div className='text-gray-700 text-sm'>
+            <p className='border p-2 font-bold text-center mt-2 bg-green-300'>Code: {cca3}</p>
             <p>Region: {region? region: subregion}</p>
             <p>Capital: {capital[0]}</p>
             <p>Population: {population}</p>
+            <button onClick={()=> handleVisited()} className='btn btn-success text-white'>Visited</button>
+            {visited && <div class="bg-purple-600 text-white py-1 text-center rounded-full mt-2 text-xs">I have visited this country</div>}
             </div>
         </div>
     );
